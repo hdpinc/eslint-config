6cd3c15208ce9cff162dac8f524bc90c9732a884
ysgk <ysgk@users.noreply.github.com>
2019-09-13 18:39:12 +0900

    first

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['@typescript-eslint', 'import', 'react-hooks', 'jest'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'jest/consistent-test-it': ['error', { fn: 'it' }],
+    'no-console': 'off',
+
+    // TS で interface を export すると no-undef エラーが出る
+    // https://github.com/eslint/typescript-eslint-parser/issues/437
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'one-var': ['error', 'never'],
+    'react/display-name': 'off',
+    'react/jsx-curly-brace-presence': ['error', { props: 'always' }],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
