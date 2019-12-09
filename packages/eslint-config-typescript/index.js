1cd4185fe380fa3d4f4b99e378a95b3c0837f45a
ysgk <ysgk@users.noreply.github.com>
2019-12-09 17:08:54 +0900

    typescript

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  plugins: ['@typescript-eslint', 'import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    curly: 'error',
+    'no-console': 'off',
+
+    // TS で interface を export すると no-undef エラーが出る
+    // https://github.com/eslint/typescript-eslint-parser/issues/437
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+
+    // enum や namespace みたいな TS 独自構文は使わない
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSEnumDeclaration',
+        message: "Don't declare enums",
+      },
+      {
+        selector: 'TSModuleDeclaration',
+        message: "Don't declare module or namespace",
+      },
+    ],
+    'one-var': ['error', 'never'],
+  },
+}
