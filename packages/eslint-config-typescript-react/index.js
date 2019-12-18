72b944645c3846fda9539cfc2802bdf3d0a5d384
ysgk <ysgk@users.noreply.github.com>
2019-12-18 14:41:10 +0900

    eslint-config-typescript を extend

--- packages/eslint-config-typescript-react/index.js
+++ packages/eslint-config-typescript-react/index.js
@@ -1,39 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  plugins: ['@typescript-eslint', 'import', 'react-hooks'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-  },
+  extends: ['@jmdc/eslint-config-typescript', 'plugin:react/recommended'],
+  plugins: ['react-hooks'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    curly: 'error',
-    'no-console': 'off',
-    'no-extra-semi': 'off',
-
-    // TS で interface を export すると no-undef エラーが出る
-    // https://github.com/eslint/typescript-eslint-parser/issues/437
-    'no-undef': 'off',
-    'no-unused-vars': 'off',
-
-    // enum や namespace みたいな TS 独自構文は使わない
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'TSEnumDeclaration',
-        message: "Don't declare enums",
-      },
-      {
-        selector: 'TSModuleDeclaration',
-        message: "Don't declare module or namespace",
-      },
-    ],
-    'one-var': ['error', 'never'],
     'react/display-name': 'off',
     'react/jsx-curly-brace-presence': ['error', { props: 'always' }],
     'react-hooks/exhaustive-deps': 'warn',
